e06d2c76951c49aacdc89b56da7f07ab07114d63
I745724 <simona.pacheliyska@sap.com>
2025-01-11 10:08:54 +0200

    Add logo, fix styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "../styles/Navbar.css";
 import { CiLogout } from "react-icons/ci";
+import medisynergy from "../assets/medicalsynergy.png";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-left">
+      <div className="navbar-container">
+        <img src={medisynergy} className="app-logo" alt="logo" />
         <a href="/" className="logo">
           Medical Synergy
         </a>
       </div>
-      <div className="navbar-right">
+      <div className="navbar-container">
         <ul className="nav-links">
           <a href="/my-medications">My medications</a>
           <a href="/profile">Profile</a>
